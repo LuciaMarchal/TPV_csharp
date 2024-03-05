75fd4c28020a6c3c16a323b9f39d21a3c158e1fd
LuciaMarchal <luciamrchl@gmail.com>
2024-03-05 09:40:29 +0100

    creacion clase Fichaje

---
+++ ProyectoTrimestral/Clases/Fichaje.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTrimestral.Clases
+{
+    public class Fichaje
+    {
+        public Empleado empleado { get; set; }
+        public DateTime hora { get; set; }
+
+        public Fichaje(Empleado empleado, DateTime fichaje)
+        {
+            this.empleado = empleado;
+            this.hora = fichaje;
+        }
+
+        public Fichaje()
+        {
+        }
+    }
+}
